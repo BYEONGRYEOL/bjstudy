d1cd9fe7b1dc704a54bf473b7e776b57d1da381d
BYEONGRYEOL <lyulbyung@gmail.com>
2023-09-30 14:51:53 +0900

    연휴동안 공부한거

--- src/main/java/org/example/계단오르기_2579.java
+++ src/main/java/org/example/계단오르기_2579.java
@@ -3,47 +3,23 @@ package org.example;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class 평범한배낭_12865 {
+public class 계단오르기_2579 {
 
     static void input() throws Exception {
         int n = scan.nextInt();
-        int k = scan.nextInt();
-        int[] w = new int[n+1];
-        int[] v = new int[n+1];
-        for(int i = 1 ; i <= n ; i ++){
-            w[i] = scan.nextInt();
-            v[i] = scan.nextInt();
-        }
-        // 무게 제한을 늘려가며 과거의 답에서 현재의 답을 도출
-        // 각각의 인덱스는 검사중인 물건을 뜻함
-        // 무게제한 1
-        // 0 0 0 0
-        // 무게제한 2
-        // 0 0 0 0
-        // 무게제한 3
-        // 0 0 6 6
-        // 무게제한 4
-        // 0 8 8 8
-        // 무게제한 5
-        // 0 8 8 12
-        // 무게제한 6
-        // 13 13 13 13
-        // 무게제한 7
-        // 13 13 14 14 무게제한이 3일 적에도 6을 담았었으니
-        int[][] dp = new int[n+1][k+1];
-        for(int i = 1; i <=n; i++){
-            for(int j = 1; j <= k ; j ++){
-                if(w[i] > j) //담을 수 없다면 이전 최댓값 불러오기
-                    dp[i][j] = dp[i-1][j];
-                else{
-                    // 담을 수 있다면 이전 최댓값과,
-                    // 이전에 현재 물건을 담기 전 무게제한일 때의 최댓값과 현재 물건의 가치를 더한 값을 비교
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - w[i]] +v[i]);
-                }
-            }
-        }
-        System.out.println(dp[n][k]);
+        int stair[] = new int[n+1];
+        for(int i = 1 ; i < n+1 ; i++)
+            stair[i] = scan.nextInt();
+        int dp[] = new int[n+1];
+        dp[1] = stair[1];
+        dp[2] = dp[1] + stair[2];
+        dp[3] = Math.max(stair[1] + stair[3], stair[2] + stair[3]);
 
+
+        for(int i =4; i<=n; i++){
+            dp[i] = Math.max(dp[i-3] + stair[i-1] + stair[i], dp[i-2] + stair[i]);
+        }
+        System.out.println(dp[n]);
     }
 
     static void print(){
--- src/main/java/org/example/피보나치함수_1003.java
+++ src/main/java/org/example/피보나치함수_1003.java
@@ -3,14 +3,14 @@ package org.example;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class 평범한배낭_12865 {
+public class 피보나치함수_1003 {
 
     static void input() throws Exception {
         int n = scan.nextInt();
         int k = scan.nextInt();
-        int[] w = new int[n+1];
-        int[] v = new int[n+1];
-        for(int i = 1 ; i <= n ; i ++){
+        int[] w = new int[n];
+        int[] v = new int[n];
+        for(int i = 0 ; i < n ; i ++){
             w[i] = scan.nextInt();
             v[i] = scan.nextInt();
         }
@@ -30,15 +30,14 @@ public class 평범한배낭_12865 {
         // 13 13 13 13
         // 무게제한 7
         // 13 13 14 14 무게제한이 3일 적에도 6을 담았었으니
-        int[][] dp = new int[n+1][k+1];
-        for(int i = 1; i <=n; i++){
-            for(int j = 1; j <= k ; j ++){
-                if(w[i] > j) //담을 수 없다면 이전 최댓값 불러오기
+        int[][] dp = new int[k+1][n];
+        for(int i = 1; i <=k; i++){
+            for(int j = 0; j < n ; j ++){
+                if(w[j] > k) //담을 수 없다면 이전 최댓값 불러오기
                     dp[i][j] = dp[i-1][j];
                 else{
-                    // 담을 수 있다면 이전 최댓값과,
-                    // 이전에 현재 물건을 담기 전 무게제한일 때의 최댓값과 현재 물건의 가치를 더한 값을 비교
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - w[i]] +v[i]);
+                    // 담을 수 있다면
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - w[j]] +v[j]);
                 }
             }
         }
