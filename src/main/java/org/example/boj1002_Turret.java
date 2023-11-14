01f5f191d5949e83bbd75dc3f3b71b2ef0ec8233
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-14 10:11:54 +0900

    코드카타 1-2, 1-3

--- src/main/java/org/example/boj1002_Turret.java
+++ src/main/java/org/example/boj1002_Turret.java
@@ -10,35 +10,28 @@ import java.util.StringTokenizer;
 public class boj1002_Turret {
 
     static void input(){
-        int caseNum = scan.nextInt();
-        for(int z = 0; z < caseNum; z++){
-            // 문제 조건 정리
-            int x1 = scan.nextInt();
-            int y1 = scan.nextInt();
-            int r1 = scan.nextInt();
-            int x2 = scan.nextInt();
-            int y2 = scan.nextInt();
-            int r2 = scan.nextInt();
+        int caseNum= scan.nextInt();
+        for(int z = 0; z < caseNum ; z ++){
+            int open = 0;
+            String s = scan.nextLine();
+            boolean isRightParenthesis = true;
+            for(int i = 0; i < s.length(); i++){
+                char parenthesis = s.charAt(i);
+                if(parenthesis == '(')
+                    open++;
+                else{
+                    if(open == 0){
+                        isRightParenthesis = false;
+                        break;
+                    }
+                    open --;
 
-            // 문제 이해를 위해 그림을 그려보니까
-            // 두 원이 몇개의 점에서 만나는지를 물어보는 것이다.
-            // 두 원의 중심 사이의 거리가 중요
-            double d_CenterOfCircle = Math.pow((x1-x2) * (x1-x2) + (y1-y2) * (y1-y2), 0.5);
-
-            if(d_CenterOfCircle == 0 && r1 == r2) // case6 겹침
-                sb.append(-1);
-            else if(d_CenterOfCircle > r1+r2) //case5 멀어서 안만남
-                sb.append(0);
-            else if(d_CenterOfCircle < Math.abs(r1-r2)) //case2 포함관계에 안만남
-                sb.append(0);
-            else if(d_CenterOfCircle == Math.abs(r1-r2)) // case3 내접
-                sb.append(1);
-            else if(d_CenterOfCircle == r1+r2) // case4 외접
-                sb.append(1);
-            else // case1 나머지는 두점에서 만남
-                sb.append(2);
-            //테스트케이스간 줄넘김
-            sb.append('\n');
+                }
+            }
+            if(isRightParenthesis && open ==0)
+                sb.append("YES").append('\n');
+            else
+                sb.append("NO").append('\n');
         }
         print();
     }
