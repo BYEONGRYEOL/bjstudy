eb9aad612d2d681f7202fd64cb44fe948c983986
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-21 09:32:56 +0900

    코드카타 16 ATM

--- src/main/java/org/example/boj11399_ATM.java
+++ src/main/java/org/example/boj11399_ATM.java
@@ -7,21 +7,19 @@ import java.util.StringTokenizer;
 public class boj11399_ATM {
 
     static void input() throws Exception {
+
+        // SJF shortest job first
         // 당연히 작업 시간이 짧은 사람부터 out되어야 한다.
         int n = scan.nextInt();
         int[] array = new int[n];
         for(int i = 0 ; i<  n ; i++)
             array[i] = scan.nextInt();
+        Arrays.sort(array);
 
-        Arrays.sort(array); // 오름차순
-
-        int answer = 0;
-        int waitingTime = 0;
-        for(int i = 0 ; i < n ; i++){
-            waitingTime += array[i];
-            answer += waitingTime;
-        }
-        System.out.println(answer);
+        // 누적합(적분과 비슷함)
+        for(int i = 1; i < n; i++)
+            array[i] += array[i-1];
+        System.out.println(Arrays.stream(array).sum());
     }
 
     static void print(){
