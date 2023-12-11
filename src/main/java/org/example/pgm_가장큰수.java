310e3d4a657fbefbfbd3095fd2ef36e4523973a9
BYEONGRYEOL <lyulbyung@gmail.com>
2023-12-11 12:56:54 +0900

    코드카타 1211

---
+++ src/main/java/org/example/pgm_가장큰수.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.stream.*;
+class pgm_가장큰수 {
+	public static String solution(int[] numbers) {
+		if(IntStream.of(numbers).sum() == 0) return "0";
+		return IntStream.of(numbers)
+			.mapToObj(String::valueOf)
+			.sorted((s1,s2)->(s2 + s1).compareTo(s1 + s2))
+			.collect(Collectors.joining());
+	}
+
+	public static void main(String[] args) {
+		solution(new int[] {1, 4, 25, 342, 102, 2});
+	}
+}
