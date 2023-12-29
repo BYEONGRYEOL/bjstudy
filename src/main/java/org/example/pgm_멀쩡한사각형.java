8cfe0de97295d0921558ad06840c2bee0eff6879
BYEONGRYEOL <lyulbyung@gmail.com>
2023-12-29 10:03:17 +0900

    1229 코드카타

---
+++ src/main/java/org/example/pgm_멀쩡한사각형.java
@@ -0,0 +1,12 @@
+package org.example;
+
+class pgm_멀쩡한사각형 {
+	int gcd(int a, int b) {
+		return a % b == 0 ? b : gcd(b, a % b);
+	}
+
+	public long solution(int w, int h) {
+
+		return (long)w * h - (w + h - gcd(w, h));
+	}
+}
