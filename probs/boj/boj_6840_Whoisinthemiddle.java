7f98412554421275299fda9ea9802e54cb9826e1
BYEONGRYEOL <lyulbyung@gmail.com>
2024-10-01 20:23:22 +0900

    boj_6840_Whoisinthemiddle 정렬

---
+++ probs/boj/boj_6840_Whoisinthemiddle.java
@@ -0,0 +1,81 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_6840_Whoisinthemiddle {
+
+	static void input() throws Exception {
+		int[] array = new int[3];
+		array[0] = scan.nextInt();
+		array[1] = scan.nextInt();
+		array[2] = scan.nextInt();
+		Arrays.sort(array);
+		System.out.println(array[1]);
+
+	}
+
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
