02aa63b31c6197b30b60f27e693576a705c90dc9
BYEONGRYEOL <lyulbyung@gmail.com>
2024-10-02 08:18:44 +0900

    boj_11654_아스키코드 형변환

---
+++ probs/boj/boj_11654_아스키코드.java
@@ -0,0 +1,76 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_11654_아스키코드 {
+
+	static void input() throws Exception {
+		char c = scan.next().charAt(0);
+		System.out.println((int)c);
+	}
+
+
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
