3700aea6ed92cc8d2bcfd8056722a24a91166f30
BYEONGRYEOL <lyulbyung@gmail.com>
2024-10-01 20:19:34 +0900

    boj_5522_카드게임 반복

---
+++ probs/boj/boj_5522_카드게임.java
@@ -0,0 +1,80 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj_5522_카드게임 {
+
+	static void input() throws Exception {
+		int result = 0;
+		for (int i = 0; i < 5; i++) {
+			result += scan.nextInt();
+		}
+		System.out.println(result);
+	}
+
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
