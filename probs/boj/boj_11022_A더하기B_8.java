4ba4d99fd2fec88cad330b627d9b8305d627f9ec
BYEONGRYEOL <lyulbyung@gmail.com>
2024-10-01 22:44:15 +0900

    boj_11022_A더하기B_8 문자열 기본함수

---
+++ probs/boj/boj_11022_A더하기B_8.java
@@ -0,0 +1,81 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_11022_A더하기B_8 {
+
+	static void input() throws Exception {
+		int iter = scan.nextInt();
+		for (int i = 0; i < iter; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			sb.append(String.format("Case #%d: %d + %d = %d", i+1, a,b,a+b)).append('\n');
+		}
+		print();
+	}
+
+
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
