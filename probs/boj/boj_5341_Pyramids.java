4f59b7d65087027b5ed14c14b4962581e5e59a18
BYEONGRYEOL <lyulbyung@gmail.com>
2024-10-01 20:16:11 +0900

    boj_5341_Pyramids IntStream

---
+++ probs/boj/boj_5341_Pyramids.java
@@ -0,0 +1,86 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class boj_5341_Pyramids {
+	// IntStream.iterate(시작값, 지속조건, 다음 반복숫자) 로 아래의 내용을 대체함
+	// while( num > 0 ){
+	// 	  result += num;
+	//    num --;
+	// }
+
+	static void input() throws Exception {
+		while (true) {
+			int num = scan.nextInt();
+			if (num == 0) {
+				break;
+			}
+			sb.append(IntStream.iterate(num, n -> n > 0, n -> n - 1).sum()).append('\n');
+		}
+		print();
+	}
+
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(s));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
