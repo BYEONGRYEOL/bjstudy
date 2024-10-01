a80ba0d128dfbfbec2c3578c31b1866800b35cb6
BYEONGRYEOL <lyulbyung@gmail.com>
2024-10-01 20:05:44 +0900

    boj_5339_콜센터 출력

---
+++ probs/boj/boj_5339_콜센터.java
@@ -0,0 +1,83 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_5339_콜센터 {
+
+	static void input() throws Exception {
+		System.out.println(""
+			+ "     /~\\\n"
+			+ "    ( oo|\n"
+			+ "    _\\=/_\n"
+			+ "   /  _  \\\n"
+			+ "  //|/.\\|\\\\\n"
+			+ " ||  \\ /  ||\n"
+			+ "============\n"
+			+ "|          |\n"
+			+ "|          |\n"
+			+ "|          |");
+	}
+
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(s));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
