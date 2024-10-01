dab828f3bc6ba2d5f0804288434aa12c20fbee65
BYEONGRYEOL <lyulbyung@gmail.com>
2024-10-01 21:49:17 +0900

    boj_10807_개수세기 배열

---
+++ probs/boj/boj_10807_개수세기.java
@@ -0,0 +1,88 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class boj_10807_개수세기 {
+
+	static void input() throws Exception {
+		int iter = scan.nextInt();
+		int[] array = new int[iter];
+		for (int i = 0; i < iter; i++) {
+			array[i] = scan.nextInt();
+		}
+		int num = scan.nextInt();
+		int equalCount = 0;
+		for (int i = 0; i < array.length; i++) {
+			if(num == array[i])
+				equalCount++;
+		}
+		System.out.println(equalCount);
+	}
+
+
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
