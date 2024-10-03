612c9c8599dc3865b79a3c5f0732c714e1a0934b
BYEONGRYEOL <lyulbyung@gmail.com>
2024-10-03 22:15:45 +0900

    boj_14681_사분면고르기 삼항연산자로 풀수있는지?

---
+++ probs/boj/boj_14681_사분면고르기.java
@@ -0,0 +1,86 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_14681_사분면고르기 {
+
+	static void input() throws Exception {
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		if( x > 0)
+			if(y > 0)
+				System.out.println(1);
+			else
+				System.out.println(4);
+		else
+			if(y>0)
+				System.out.println(2);
+			else
+				System.out.println(3);
+	}
+
+
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
