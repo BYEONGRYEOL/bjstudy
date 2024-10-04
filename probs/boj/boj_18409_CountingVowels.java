2710e73500bb7cb519c780d143af40a99b9db7f7
BYEONGRYEOL <lyulbyung@gmail.com>
2024-10-04 18:42:06 +0900

    boj_18409_CountingVowels 미리 배열 선언하기

---
+++ probs/boj/boj_18409_CountingVowels.java
@@ -0,0 +1,85 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_18409_CountingVowels {
+	static char[] vowelArray = {'a','e','i','o','u','A','E','I','O','U'};
+	static void input() throws Exception {
+		scan.nextInt();
+		String line = scan.nextLine();
+		int count = 0;
+		for (char c : line.toCharArray()) {
+			for (int i = 0; i < 10; i++) {
+				if(vowelArray[i] == c){
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		input();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
