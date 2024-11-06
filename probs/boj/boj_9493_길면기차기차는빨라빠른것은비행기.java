ca999364b1c28d9aa6e53b5ec0e654b8a1d4002c
BYEONGRYEOL <lyulbyung@gmail.com>
2024-11-06 11:26:01 +0900

    ..

---
+++ probs/boj/boj_9493_길면기차기차는빨라빠른것은비행기.java
@@ -0,0 +1,150 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_9493_길면기차기차는빨라빠른것은비행기 {
+    static void solve() throws Exception {  
+		while(true){
+			int distance = scan.nextInt();
+			int trainSpeed = scan.nextInt();
+			int airplaneSpeed = scan.nextInt();
+
+			if(distance == 0 ) break;
+			double trainSec = distance/(double)trainSpeed * 3600;
+			double airplaneSec = distance/(double)airplaneSpeed * 3600;
+			sb.append(toTimeString(trainSec - airplaneSec)).append('\n');
+		}
+	}
+	static String toTimeString(double sec){
+		long secInt = Math.round(sec);
+		return String.format("%d:%02d:%02d", secInt / 3600, (secInt % 3600 ) / 60, secInt % 60);
+	}
+
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray(int n) {
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = nextInt();
+			}
+			return arr;
+		}
+
+		long[] nextLongArray(int n) {
+			long[] arr = new long[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = nextLong();
+			}
+			return arr;
+		}
+		long[][] nextLongMatrix(int n, int m) {
+			long[][] arr = new long[n][m];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					arr[i][j] = nextLong();
+				}
+			}
+			return arr;
+		}
+
+		public double[] nextDoubleArray(int n) {
+			double[] arr = new double[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = nextDouble();
+			}
+			return arr;
+		}
+
+		double[][] nextDoubleMatrix(int n, int m) {
+			double[][] arr = new double[n][m];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					arr[i][j] = nextDouble();
+				}
+			}
+			return arr;
+		}
+
+		int[][] nextIntMatrix(int n) {
+			return nextIntMatrix(n, n);
+		}
+
+		int[][] nextIntMatrix(int n, int m) {
+			int[][] arr = new int[n][m];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					arr[i][j] = nextInt();
+				}
+			}
+			return arr;
+		}
+
+
+		char[] nextCharArrayFromString() {
+			return scan.nextLine().toCharArray();
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		solve();
+		print();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
