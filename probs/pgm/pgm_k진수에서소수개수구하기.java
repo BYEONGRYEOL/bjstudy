586779219b96bb516c923b4ecfeaa46f6770ae8d
BYEONGRYEOL <lyulbyung@gmail.com>
2025-03-22 20:50:53 +0900

    ,

--- probs/pgm/pgm_k진수에서소수개수구하기.java
+++ probs/pgm/pgm_k진수에서소수개수구하기.java
@@ -2,22 +2,23 @@ package probs.pgm;
 
 import java.util.*;
 import java.util.stream.*;
+
 class pgm_k진수에서소수개수구하기 {
-	public String toBase_KNumber(int n, int k){
+	public String toBase_KNumber(int n, int k) {
 		StringBuilder sb = new StringBuilder();
 
-		while(n >0){
-			sb.append(n%k);
-			n/=k;
+		while (n > 0) {
+			sb.append(n % k);
+			n /= k;
 		}
 		return sb.reverse().toString();
 	}
 
-	public boolean isPrime(long number){
-		if(number == 1)
+	public boolean isPrime(long number) {
+		if (number == 1)
 			return false;
-		for(int i = 2; i <= Math.pow(number, 0.5); i++){
-			if(number % i == 0)
+		for (int i = 2; i <= Math.pow(number, 0.5); i++) {
+			if (number % i == 0)
 				return false;
 		}
 		return true;
@@ -27,7 +28,8 @@ class pgm_k진수에서소수개수구하기 {
 		// 0을 포함한 소수는 제외
 		// 먼저 k진수로 변환하는 코드 작성
 		int answer = -1;
-		String kBase = toBase_KNumber(n,k);
-		return (int) Arrays.stream(kBase.split("0")).filter(str -> "".equals(str) == false).map(Long::parseLong).filter(l -> isPrime(l)).count();
+		String kBase = toBase_KNumber(n, k);
+		return (int) Arrays.stream(kBase.split("0")).filter(str -> "".equals(str) == false).map(Long::parseLong)
+				.filter(l -> isPrime(l)).count();
 	}
 }
