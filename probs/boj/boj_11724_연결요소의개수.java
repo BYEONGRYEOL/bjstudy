270730032f1ed7bb4f45dbc80c20a87d51c84d1d
BYEONGRYEOL <lyulbyung@gmail.com>
2025-08-22 22:36:45 +0900

    .

---
+++ probs/boj/boj_11724_연결요소의개수.java
@@ -0,0 +1,177 @@
+package probs.boj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj_11724_연결요소의개수 {
+    static void solve() throws Exception {
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		int[][] graph = new int[n + 1][n + 1];
+		for (int i = 0; i < m; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			graph[a][b] = 1;
+			graph[b][a] = 1;
+		}
+
+		boolean[] visited = new boolean[n + 1];
+		int componentCount = 0;
+		Queue<Integer> queue = new java.util.LinkedList<>();
+		
+		for (int i = 1; i <= n; i++) {
+			if (!visited[i]) { // Not visited
+				componentCount++;
+				queue.offer(i);
+				visited[i] = true; // Mark as visited
+				while (!queue.isEmpty()) {
+					int current = queue.poll();
+					for (int j = 1; j <= n; j++) {
+						if (graph[current][j] == 1 && !visited[j]) {
+							queue.offer(j);
+							visited[j] = true;
+						}
+					}
+				}
+			}
+		}
+		sb.append(componentCount).append("\n");
+	}
+	
+	static void print() {
+		System.out.print(sb.toString());
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] nextIntArray(int n) {
+			int[] arr = new int[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = nextInt();
+			}
+			return arr;
+		}
+
+		long[] nextLongArray(int n) {
+			long[] arr = new long[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = nextLong();
+			}
+			return arr;
+		}
+		long[][] nextLongMatrix(int n, int m) {
+			long[][] arr = new long[n][m];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					arr[i][j] = nextLong();
+				}
+			}
+			return arr;
+		}
+
+		String[][] nextStringMatrix_space(int n, int m) {
+			String[][] arr = new String[n][m];
+			for (int i = 0; i < n; i++) {
+				arr[i] = scan.nextLine().split(" ");
+			}
+			return arr;
+		}
+
+		public double[] nextDoubleArray(int n) {
+			double[] arr = new double[n];
+			for (int i = 0; i < n; i++) {
+				arr[i] = nextDouble();
+			}
+			return arr;
+		}
+
+		double[][] nextDoubleMatrix(int n, int m) {
+			double[][] arr = new double[n][m];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					arr[i][j] = nextDouble();
+				}
+			}
+			return arr;
+		}
+
+		int[][] nextIntMatrix(int n) {
+			return nextIntMatrix(n, n);
+		}
+
+		int[][] nextIntMatrix(int n, int m) {
+			int[][] arr = new int[n][m];
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					arr[i][j] = nextInt();
+				}
+			}
+			return arr;
+		}
+
+
+		char[] nextCharArrayFromString() {
+			return scan.nextLine().toCharArray();
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		solve();
+		print();
+	}
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+}
